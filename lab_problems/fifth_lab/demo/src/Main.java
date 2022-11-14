72e5870a3e45eb4358d766a00aeb019b5685536d
Giorno <garcovi.izzi@gmail.com>
2022-11-14 16:22:10 +0200

    forth lab, fifth not done quite well

--- lab_problems/fifth_lab/demo/src/Main.java
+++ lab_problems/fifth_lab/demo/src/Main.java
@@ -7,15 +7,32 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         User[] users = new User[10];
         answer = scan.nextLine();
-        if(answer.equals("yes")){
+        if (answer.equals("yes")) {
             String username;
             System.out.println("Enter username");
-            username = scan.nextLine()
+            username = scan.nextLine();
             User curr;
             curr = Validators.checkUser(username, users);
-
+            if (curr == null) {
+                System.out.println("There is no such user");
+            }
+        } else {
+            String curr_username;
+            String password;
+            String number;
+            System.out.println("Enter username");
+            curr_username = scan.nextLine();
+            System.out.println("Enter password");
+            password = scan.nextLine();
+            System.out.println("Enter number");
+            number = scan.nextLine();
+            User newUser = new User(curr_username, password, number);
+            for (int i = 0; i < 10; ++i) {
+                if (users[i] == null) {
+                    users[i] = newUser;
+                }
+            }
         }
-
 
 
     }
